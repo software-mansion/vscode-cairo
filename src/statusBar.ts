7f5f5c705146147de5cca1b16aa8d4e911b275f8
Arcticae <tomekgsd@gmail.com>
2025-02-28 12:06:46 +0100

    React to analyzing notifications with a spinner in the status bar

--- src/statusBar.ts
+++ src/statusBar.ts
@@ -4,11 +4,16 @@ import type { Context } from "./context";
 import { toolchainInfo } from "./lspRequests";
 import { Scarb } from "./scarb";
 import { timeout } from "promise-timeout";
+import { CairoExtensionManager } from "./extensionManager";
 
 const CAIRO_STATUS_BAR_COMMAND = "cairo1.statusBar.clicked";
 
+const CAIRO_STATUS_BAR_TXT = "Cairo";
+const SPINNER = "$(loading~spin)";
+const CAIRO_STATUS_BAR_SPINNING = `${CAIRO_STATUS_BAR_TXT} ${SPINNER}`;
+
 export class StatusBar {
-  private statusBarItem: vscode.StatusBarItem;
+  private readonly statusBarItem: vscode.StatusBarItem;
   private client?: lc.LanguageClient | undefined;
 
   constructor(private readonly context: Context) {
@@ -17,8 +22,42 @@ export class StatusBar {
     this.context.extension.subscriptions.push(this.statusBarItem);
   }
 
-  public async setup(client?: lc.LanguageClient): Promise<void> {
-    this.client = client;
+  private startSpinning() {
+    this.statusBarItem.text = CAIRO_STATUS_BAR_SPINNING;
+  }
+
+  private stopSpinning() {
+    this.statusBarItem.text = CAIRO_STATUS_BAR_TXT;
+  }
+
+  private subscribeToStatusNotifications() {
+    this.context.extension.subscriptions.push(
+      this.client!.onNotification(
+        "cairo/serverStatus",
+        (serverStatusParams: { event: string; idle: boolean }) => {
+          const { idle } = serverStatusParams;
+          if (idle) {
+            this.stopSpinning();
+          } else {
+            this.startSpinning();
+          }
+        },
+      ),
+    );
+  }
+
+  private handleClientChange(newClient?: lc.LanguageClient) {
+    this.client = newClient;
+    if (newClient) {
+      this.subscribeToStatusNotifications();
+    }
+  }
+
+  public async setup(extensionManager: CairoExtensionManager): Promise<void> {
+    // Handle initial client value
+    this.handleClientChange(extensionManager.getClient());
+
+    extensionManager.onNewClient(this.handleClientChange);
 
     this.context.extension.subscriptions.push(
       vscode.workspace.onDidChangeConfiguration(() => {
@@ -28,6 +67,7 @@ export class StatusBar {
 
     this.context.extension.subscriptions.push(
       vscode.commands.registerCommand(CAIRO_STATUS_BAR_COMMAND, () => {
+        const client = this.client;
         if (client) {
           client.outputChannel.show();
         } else {
@@ -41,7 +81,7 @@ export class StatusBar {
 
   private async initializeStatusBarItem(): Promise<void> {
     this.statusBarItem.command = CAIRO_STATUS_BAR_COMMAND;
-    this.statusBarItem.text = "Cairo";
+    this.statusBarItem.text = CAIRO_STATUS_BAR_TXT;
     this.statusBarItem.tooltip = "Cairo Language";
 
     await this.updateScarbVersion();
