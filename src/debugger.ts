bb610f386837fe64a2dbfc08fd78c61465182064
Dariusz Doktorski <dariusz.doktorski@swmansion.com>
2025-11-28 13:36:17 +0100

    Add debugger logs

--- src/debugger.ts
+++ src/debugger.ts
@@ -5,7 +5,10 @@ import * as readline from "node:readline";
 import { Readable } from "node:stream";
 
 export function enableLaunchingDebugger(ctx: Context) {
-  const factory = new CairoDebugAdapterServerDescriptorFactory();
+  const debuggerLog = vscode.window.createOutputChannel("Cairo Debugger");
+  ctx.extension.subscriptions.push(debuggerLog);
+
+  const factory = new CairoDebugAdapterServerDescriptorFactory(debuggerLog);
   ctx.extension.subscriptions.push(
     vscode.debug.registerDebugAdapterDescriptorFactory("cairo", factory),
   );
@@ -14,12 +17,24 @@ export function enableLaunchingDebugger(ctx: Context) {
 class CairoDebugAdapterServerDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
   private debugAdapterProcesses: ChildProcess[] = [];
 
+  constructor(private readonly debuggerLog: vscode.OutputChannel) {}
+
   createDebugAdapterDescriptor(
     session: vscode.DebugSession,
   ): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+    this.debuggerLog.show(true);
+
     const program = session.configuration["program"] as string;
-    const adapterProcess = spawn(program, { stdio: "pipe", env: process.env });
+    const adapterProcess = spawn(program, {
+      stdio: "pipe",
+      env: { ...process.env, CAIRO_DEBUGGER_LOG: "trace" },
+    });
+
     this.debugAdapterProcesses.push(adapterProcess);
+
+    adapterProcess.stderr.on("data", (data: Buffer) => {
+      this.debuggerLog.append(data.toString());
+    });
 
     return this.waitForFreePort(adapterProcess.stdout).then(
       (port) => new vscode.DebugAdapterServer(port),
